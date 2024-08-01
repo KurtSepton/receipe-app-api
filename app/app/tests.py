1524c1a18d84be82c414b65573f38c9d66ba9529
Kurt Septon <kurt.septon@ookla.com>
2024-08-01 14:44:23 -0600

    adding sql stuff for docker

---
+++ app/app/tests.py
@@ -0,0 +1,23 @@
+"""
+sample tests
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+
+    def test_add_numbers(self):
+        """Test that two numbers are added together"""
+
+        result = calc.add(5, 6)
+
+        self.assertEqual(result, 11)
+
+    def test_subtract_numbers(self):
+        """Test that values are subtracted and returned"""
+
+        result = calc.subtract(10, 15)
+
+        self.assertEqual(result, 5)
